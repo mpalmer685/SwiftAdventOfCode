88b2444c7d943d5539c1a75fb6fa90b133e99c13
Mike Palmer <mrpalmer685@gmail.com>
2020-12-01 12:17:39 -0500

    Simplify puzzle registration

--- Sources/AOCKit/PuzzleDay.swift
+++ Sources/AOCKit/PuzzleDay.swift
@@ -3,9 +3,8 @@ public struct PuzzleDay {
     let part1: StringPuzzleSolution
     let part2: StringPuzzleSolution
 
-    public static func day<T: Puzzle>(_ day: UInt8, _ puzzleType: T.Type) -> PuzzleDay {
-        let puzzle = T()
-        return PuzzleDay(
+    public static func day<T: Puzzle>(_ day: UInt8, _ puzzle: T) -> PuzzleDay {
+        PuzzleDay(
             day: day,
             part1: wrap { try puzzle.part1Solution(for: $0) },
             part2: wrap { try puzzle.part2Solution(for: $0) }
