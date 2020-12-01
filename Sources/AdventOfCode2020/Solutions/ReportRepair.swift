905e343ebb86572e164e9a0249cac92f278a4d67
Mike Palmer <mrpalmer685@gmail.com>
2020-12-01 12:45:17 -0500

    Day 1 solution

---
+++ Sources/AdventOfCode2020/Solutions/ReportRepair.swift
@@ -0,0 +1,46 @@
+import AOCKit
+
+struct ReportRepair: Puzzle {
+    func part1Solution(for input: String) throws -> Int {
+        let entries = getLines(from: input).compactMap(Int.init)
+        guard let (first, second) = findPair(totaling: 2020, in: entries) else {
+            throw ReportRepairError.noMatchesFound
+        }
+        return first * second
+    }
+
+    func part2Solution(for input: String) throws -> Int {
+        let entries = getLines(from: input).compactMap(Int.init).sorted()
+        for (i, entry) in entries.enumerated() {
+            let goalTotal = 2020 - entry
+            if let (first, second) = findPair(totaling: goalTotal, in: entries[i + 1 ..< entries.endIndex]) {
+                return entry * first * second
+            }
+        }
+        throw ReportRepairError.noMatchesFound
+    }
+
+    private func findPair<T: Collection>(totaling goal: Int, in array: T) -> (Int, Int)? where T.Element == Int {
+        let sorted = array.sorted()
+        var low = sorted.startIndex
+        var high = sorted.endIndex - 1
+        while low < high {
+            let first = sorted[low]
+            let second = sorted[high]
+            let total = first + second
+
+            if total < goal {
+                low += 1
+            } else if total > goal {
+                high -= 1
+            } else {
+                return (first, second)
+            }
+        }
+        return nil
+    }
+}
+
+enum ReportRepairError: Error {
+    case noMatchesFound
+}
--- Sources/AdventOfCode2020/main.swift
+++ Sources/AdventOfCode2020/main.swift
@@ -1,5 +1,5 @@
 import AOCKit
 
 AOC.run(puzzles: [
-
+    .day(1, ReportRepair())
 ], resultsPath: "Results/2020.json")
