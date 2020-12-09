014d6a14849b6fe1aca0fde2ff40c083a3e654ff
Mike Palmer <mrpalmer685@gmail.com>
2020-12-09 11:04:48 -0500

    Refactor Day 1 solution to use shared helpers

--- Sources/AdventOfCode2020/Solutions/ReportRepair.swift
+++ Sources/AdventOfCode2020/Solutions/ReportRepair.swift
@@ -3,7 +3,7 @@ import AOCKit
 struct ReportRepair: Puzzle {
     func part1Solution(for input: String) throws -> Int {
         let entries = getLines(from: input).compactMap(Int.init)
-        guard let (first, second) = findPair(totaling: 2020, in: entries) else {
+        guard let (first, second) = entries.findPair(totaling: 2020) else {
             throw ReportRepairError.noMatchesFound
         }
         return first * second
@@ -13,36 +13,12 @@ struct ReportRepair: Puzzle {
         let entries = getLines(from: input).compactMap(Int.init).sorted()
         for (i, entry) in entries.enumerated() {
             let goalTotal = 2020 - entry
-            if let (first, second) = findPair(
-                totaling: goalTotal,
-                in: entries[i + 1 ..< entries.endIndex]
-            ) {
+            let collection = entries[(i + 1)...]
+            if let (first, second) = collection.findPair(totaling: goalTotal) {
                 return entry * first * second
             }
         }
         throw ReportRepairError.noMatchesFound
-    }
-
-    private func findPair<T: Collection>(totaling goal: Int, in array: T) -> (Int, Int)?
-        where T.Element == Int
-    {
-        let sorted = array.sorted()
-        var low = sorted.startIndex
-        var high = sorted.endIndex - 1
-        while low < high {
-            let first = sorted[low]
-            let second = sorted[high]
-            let total = first + second
-
-            if total < goal {
-                low += 1
-            } else if total > goal {
-                high -= 1
-            } else {
-                return (first, second)
-            }
-        }
-        return nil
     }
 }
 
