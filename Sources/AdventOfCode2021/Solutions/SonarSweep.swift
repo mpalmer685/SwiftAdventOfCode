48e251810d9aaa965c61b6d93d1189eac43e0be6
Mike Palmer <mrpalmer685@gmail.com>
2021-12-01 19:22:21 -0500

    Refactor Day 1 solution

--- Sources/AdventOfCode2021/Solutions/SonarSweep.swift
+++ Sources/AdventOfCode2021/Solutions/SonarSweep.swift
@@ -7,7 +7,7 @@ struct SonarSweep: Puzzle {
     }
 
     func part2Solution(for input: String) throws -> Int {
-        let measurements = groupMeasurements(getMeasurements(from: input), groupSize: 3)
+        let measurements = getMeasurements(from: input).windowed(3).map(sum)
         return countIncreases(in: measurements)
     }
 
@@ -17,21 +17,15 @@ struct SonarSweep: Puzzle {
 }
 
 private func countIncreases(in measurements: [Int]) -> Int {
-    measurements.enumerated().reduce(0) { total, item in
-        let (index, measurement) = item
-        let previousMeasurement = index == 0 ? measurement : measurements[index - 1]
-        let delta = measurement - previousMeasurement
-        let unitDelta = delta == 0 ? 0 : max(0, delta / abs(delta))
-        return total + unitDelta
-    }
+    measurements.indices.count { $0 > 0 && measurements[$0] > measurements[$0 - 1] }
 }
 
-private func groupMeasurements(_ measurements: [Int], groupSize: Int) -> [Int] {
-    var grouped: [Int] = []
+private func sum(_ values: [Int]) -> Int {
+    values.reduce(0, +)
+}
 
-    for i in 0 ..< measurements.count - 2 {
-        grouped.append(measurements[i] + measurements[i + 1] + measurements[i + 2])
+private extension Array {
+    func windowed(_ windowSize: Int) -> [[Element]] {
+        (0 ... count - windowSize).map { Array(self[$0 ..< $0 + windowSize]) }
     }
-
-    return grouped
 }
