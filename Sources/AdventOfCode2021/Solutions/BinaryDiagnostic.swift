a7a08fcf5002c2f820167bfefba3cf6cb0365907
Mike Palmer <mrpalmer685@gmail.com>
2021-12-04 09:19:42 -0500

    Day 3 solution

---
+++ Sources/AdventOfCode2021/Solutions/BinaryDiagnostic.swift
@@ -0,0 +1,113 @@
+import AOCKit
+
+struct BinaryDiagnostic: Puzzle {
+    func part1Solution(for input: String) throws -> Int {
+        let data = parse(input)
+        let bitCount = data[0].count
+        guard data.allSatisfy({ $0.count == bitCount }) else {
+            throw BinaryDiagnosticError.inconsistentBitCount
+        }
+
+        let gammaRateBits = try (0 ..< bitCount).map { index in
+            let digits = data.map { $0[index] }
+            let mostCommonDigit = try selectValue(in: digits, using: { $0.max(by: \.value) })
+            return String(mostCommonDigit)
+        }.joined()
+        let epsilonRateBits = try invert(gammaRateBits)
+
+        guard let gammaRate = Int(gammaRateBits, radix: 2),
+              let epsilonRate = Int(epsilonRateBits, radix: 2)
+        else {
+            throw BinaryDiagnosticError.invalidResult
+        }
+
+        return gammaRate * epsilonRate
+    }
+
+    func part2Solution(for input: String) throws -> Int {
+        let data = parse(input)
+        let bitCount = data[0].count
+        guard data.allSatisfy({ $0.count == bitCount }) else {
+            throw BinaryDiagnosticError.inconsistentBitCount
+        }
+
+        var oxygenGeneratorData = data
+        var index = 0
+        while oxygenGeneratorData.count > 1, index < bitCount {
+            let digits = oxygenGeneratorData.map { $0[index] }
+            let digitToSelect = try selectValue(
+                in: digits,
+                using: selectMax,
+                withTieBreaker: "1"
+            )
+            oxygenGeneratorData.removeAll { $0[index] != digitToSelect }
+            index += 1
+        }
+
+        var scrubberData = data
+        index = 0
+        while scrubberData.count > 1, index < bitCount {
+            let digits = scrubberData.map { $0[index] }
+            let digitToSelect = try selectValue(
+                in: digits,
+                using: selectMin,
+                withTieBreaker: "0"
+            )
+            scrubberData.removeAll { $0[index] != digitToSelect }
+            index += 1
+        }
+
+        guard let oxygenGeneratorBits = oxygenGeneratorData.first?.map(String.init),
+              let co2ScrubberBits = scrubberData.first?.map(String.init),
+              let oxygenGeneratorRating = Int(oxygenGeneratorBits.joined(), radix: 2),
+              let co2ScrubberRating = Int(co2ScrubberBits.joined(), radix: 2)
+        else {
+            throw BinaryDiagnosticError.invalidResult
+        }
+
+        return oxygenGeneratorRating * co2ScrubberRating
+    }
+
+    private func parse(_ input: String) -> [[Character]] {
+        getLines(from: input).map(Array.init)
+    }
+}
+
+private typealias ElementSelector<Key: Hashable, Value> = ([Key: Value]) -> Dictionary<Key, Value>
+    .Element?
+
+private let selectMin: ElementSelector<Character, Int> = { $0.min(by: \.value) }
+private let selectMax: ElementSelector<Character, Int> = { $0.max(by: \.value) }
+
+private func selectValue(
+    in digits: [Character],
+    using select: ([Character: Int]) -> Dictionary<Character, Int>.Element?,
+    withTieBreaker tieBreaker: Character = " "
+) throws -> Character {
+    let counts: [Character: Int] = digits.reduce(into: [:]) { counts, digit in
+        counts[digit] = (counts[digit] ?? 0) + 1
+    }
+
+    if counts.allSatisfy({ $0.value == counts.first?.value }) {
+        return tieBreaker
+    }
+
+    guard let selected = select(counts)?.key else { throw BinaryDiagnosticError.computeError }
+    return selected
+}
+
+private func invert(_ binaryString: String) throws -> String {
+    func inverted(_ bit: Character) throws -> String {
+        switch bit {
+            case "0": return "1"
+            case "1": return "0"
+            default: throw BinaryDiagnosticError.invalidResult
+        }
+    }
+
+    return try Array(binaryString).map(inverted).joined()
+}
+
+private enum BinaryDiagnosticError: Error {
+    case inconsistentBitCount, invalidResult, computeError
+}
--- Sources/AdventOfCode2021/main.swift
+++ Sources/AdventOfCode2021/main.swift
@@ -3,4 +3,5 @@ import AOCKit
 AOC.run(puzzles: [
     .day(1, SonarSweep()),
     .day(2, Dive()),
+    .day(3, BinaryDiagnostic()),
 ], resultsPath: "Results/2021.json")
